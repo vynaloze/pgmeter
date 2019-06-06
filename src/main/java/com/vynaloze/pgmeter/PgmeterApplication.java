56d640e2840be2fc9b7114d93d050a0f523e753e
Piotr Pawluk <vynaloze@gmail.com>
2019-06-06 12:31:28 +0200

    initial structure

---
+++ src/main/java/com/vynaloze/pgmeter/PgmeterApplication.java
@@ -0,0 +1,13 @@
+package com.vynaloze.pgmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgmeterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgmeterApplication.class, args);
+    }
+
+}
