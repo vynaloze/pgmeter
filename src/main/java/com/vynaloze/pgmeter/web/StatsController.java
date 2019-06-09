b6420e61634e98fb2b462ceb1c0eed3f77733e89
Piotr Pawluk <vynaloze@gmail.com>
2019-06-09 18:13:46 +0200

    Add development endpoint to measure DB size

--- src/main/java/com/vynaloze/pgmeter/web/StatsController.java
+++ src/main/java/com/vynaloze/pgmeter/web/StatsController.java
@@ -8,6 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+
 @RestController
 public class StatsController {
     private final StatDao dao;
@@ -27,5 +32,29 @@ public class StatsController {
     ResponseEntity<?> getMetrics(final @PathVariable String type, final @PathVariable Long tsFrom, final @PathVariable Long tsTo) {
         final var stats = dao.get(tsFrom, tsTo, type);
         return new ResponseEntity<>(stats, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/dev/h2size")
+    ResponseEntity<?> getH2Size() {
+        final var files = new HashMap<String, Long>();
+        try {
+            Files.walkFileTree(Paths.get("/"), new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (file.endsWith("h2_data.mv.db")) {
+                        files.put(file.toString(), file.toFile().length());
+                    }
+                    return super.visitFile(file, attrs);
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.SKIP_SUBTREE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(files, HttpStatus.OK);
     }
 }
